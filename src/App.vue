3e5cb35ee7cf01828d6e8cce0a1998324cc9a56d
pillowlow <tmps9930613@gmail.com>
2021-01-21 18:32:48 +0800

    remove errors

--- src/App.vue
+++ src/App.vue
@@ -1,79 +1,36 @@
 <template>
-  
-  <div id="nav">
-    <div class="block" style="width: 10vw;">
-      開發中OuO
+  <div id="app">
+    <img src="./assets/logo.png">
+    <div>
+      <p>
+        If Element is successfully added to this project, you'll see an
+        <code v-text="'<el-button>'"></code>
+        below
+      </p>
+      <el-button>el-button</el-button>
     </div>
-    <div class="block" style="width: 14vw">
-      <router-link to="/">首頁 Home</router-link> 
-    </div>
-    <div class="block" style="width: 14vw">
-      <router-link to="/example2">關於我 About me</router-link>
-    </div>
-    <div class="block" style="width: 14vw">
-      <router-link to="/example3">作品集 Protfolio</router-link>
-    </div>
-    <div class="block" style="width: 14vw">
-      <router-link to="/example4">參與計畫</router-link>
-    </div>
-    <div class="block" style="width: 20vw">
-      <marquee >試著在空曠處點擊或拖曳滑鼠吧! try to click or drag your mouse in blank side!</marquee>
-    </div>
-    
+    <HelloWorld msg="Welcome to Your Vue.js App"/>
   </div>
-  
-  <router-view :key="$route.fullPath"/>
 </template>
+
+<script>
+import HelloWorld from './components/HelloWorld.vue'
+
+export default {
+  name: 'app',
+  components: {
+    HelloWorld
+  }
+}
+</script>
 
 <style>
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
-  color: white;
+  color: #2c3e50;
+  margin-top: 60px;
 }
-
-#nav {
-  
-  background-color: black;
-  height: 5vh;
-  width: 90vw;
-  border-style: solid;
-  border-color: white;
-  border-width:2px;
-  padding: 2px;
-  position: fixed;
-  z-index: 100;
-  margin-left: 5vw;
-  
-}
-
-#nav a {
-  font-weight: bold;
-  color: white;
-}
-
-#nav a.router-link-exact-active {
-  color: dimgray;
-}
-.block{
-  background-color: black;
-  position: relative;
-  border-width: 1px;
-  border-style: solid;
-  border-color: white;
-  padding: 2px;
-  height: 4vh;
-  text-align:center;
-  line-height: 4vh;
-  float: left;
-  margin-left: 2px;
-  
-}
-.block:hover{
-  transform: scale(1.1,1.1);
-  z-index: 10;
-}
-
 </style>
